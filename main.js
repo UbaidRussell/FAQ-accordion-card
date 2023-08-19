7577e80bf77a79c4aac3efeec61563695c86c7a4
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-08-19 16:44:24 -0400

    Added comments 📦

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const firstQuestion = document.getElementsByClassName('card-question')[0]
 const firstAnswer = document.getElementsByClassName('card-answer')[0]
 
 
-
+//This is the code for our first question
 firstQuestion.addEventListener('click', function(){
     firstQuestion.style.marginBottom = '5px';
     firstAnswer.style.display = 'block';
