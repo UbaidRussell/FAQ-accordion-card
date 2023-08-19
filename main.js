701749536945ef617eefed06434680a23d8d373f
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-08-18 22:38:06 -0400

    Fixed up our styling 🔧

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const firstQuestion = document.getElementsByClassName('card-question')[0]
+/* const firstQuestion = document.getElementsByClassName('card-question')[0]
 const firstAnswer = document.getElementsByClassName('card-answer')[0]
 
 
 
 firstQuestion.addEventListener('click', function(){
     firstAnswer.style.display = 'block';
-})
+}) */
