246bdfe6cd3d7e82dc1e823b4dc7de6f3d1f1854
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-08-20 22:20:48 -0400

    Created the second effect for our second question 📦

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const firstQuestion = document.getElementsByClassName('card-question')[0]
 const firstAnswer = document.getElementsByClassName('card-answer')[0]
-
+const secondQuestion = document.getElementsByClassName('card-question')[1]
+const secondAnswer = document.getElementsByClassName('card-answer')[1]
 
 //This is the code for our first question
 firstQuestion.addEventListener('click', function(){
@@ -9,6 +10,13 @@ firstQuestion.addEventListener('click', function(){
     firstAnswer.style.marginBottom = '10px';
     firstQuestion.style.fontWeight = 'bold';
 }) 
+secondQuestion.addEventListener('click', function(){
+    secondQuestion.style.marginBottom = '5px';
+    secondAnswer.style.display = 'block';
+    secondAnswer.style.marginBottom = '10px';
+    secondQuestion.style.fontWeight = 'bold';
+})
+
 //Work in progress for our second click on the first question to make it disappear after the first click if clicked again
 /* firstQuestion.addEventListener('dblclick', function(){
     firstQuestion.style.marginBottom = '0px';
