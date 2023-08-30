27cdbfecd3c4b5098a0c9ad47b783c8806f06132
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-08-29 21:36:46 -0400

    Got our button toggle function to work on click ⚡️

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-const firstQuestion = document.getElementsByClassName('card-question')[0]
-const firstAnswer = document.getElementsByClassName('card-answer')[0]
+/* secret-messages.js */
+const firstQuestion = document.getElementsByClassName('card-question')[0];
+const firstAnswer = document.getElementsByClassName('card-answer')[0];
 
-
-//This is the code for our first question
-firstQuestion.addEventListener('click', function(){
-    firstQuestion.style.marginBottom = '5px';
-    firstAnswer.style.display = 'block';
-    firstAnswer.style.marginBottom = '10px';
-    firstQuestion.style.fontWeight = 'bold';
-
-    //This code closes our first question when we click on it again
-    this.addEventListener('click', function(){
+const toggleHiddenElement = (domElement) => {
+    if (domElement.style.display === 'none') {
+        firstQuestion.style.marginBottom = '5px';
+        firstAnswer.style.display = 'block';
+        firstAnswer.style.marginBottom = '10px';
+        firstQuestion.style.fontWeight = 'bold';
+    
+    } else {
         firstQuestion.style.marginBottom = '20px';
         firstAnswer.style.display = 'none';
         firstAnswer.style.marginBottom = '0px';
         firstQuestion.style.fontWeight = 'normal';
-    })
-})
+    }
+}
+
+firstQuestion.addEventListener('click', () => {
+  toggleHiddenElement(firstAnswer);
+});
